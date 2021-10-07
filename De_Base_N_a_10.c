a95d315e6306922bf80351c1b635796ab0189783
ValentinGD <valenelpala@hotmail.com>
2021-10-07 15:56:03 -0300

    Amplacion main y de base n a 10 decimal

--- De_Base_N_a_10.c
+++ De_Base_N_a_10.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 void potencia(int *b, int *p, int cant);
 
-void DeNa10(int *pBase,char *pN, int *pResultadoNa10 ){
+void deNa10Entero(int *pBase,char *pN, int *pResultadoNa10 ){
 
     char *pDigito;
     pDigito=malloc(sizeof(char));
@@ -18,9 +18,13 @@ void DeNa10(int *pBase,char *pN, int *pResultadoNa10 ){
     int *pDigigitoNum;
     pDigigitoNum=malloc(sizeof(int));
 
+    while(*pN+precisionDecimal==nullptr){
+            pN--;
+    }
+
     do{
         *pDigito=*pN;
-        pN++;
+        pN--;
         if(*pDigito==('A') || *pDigito==('B') || *pDigito==('C') || *pDigito==('D') || *pDigito==('E') || *pDigito==('F')){
             *pDigigitoNum-= 'A' +10;
         }else{
@@ -32,9 +36,58 @@ void DeNa10(int *pBase,char *pN, int *pResultadoNa10 ){
         }
 
         potencia(&base,&pot, &cont);
+
         *pResultadoNa10+=*pDigito * *pPot;
         cont++;
-    }while(*n<10)
+    }while(pN>pN+precisionEntera+1)
+
+    free(pPot);
+    free(pDigito);
+    free(pDigitoLetra);
+    free(pDigigitoNum);
+    free(pCont);
+}
+
+void deNa10Decimal(int *pBase,char *pN, int *pResultadoNa10){
+
+    char *pDigito;
+    pDigito=malloc(sizeof(char));
+
+    int *pPot;
+    pPot=malloc(sizeof(int));
+
+    int *pCont;
+    pCont=malloc(sizeof(int));
+
+    char *pDigitoLetra;
+    pDigitoLetra=malloc(sizeof(char));
+
+    int *pDigigitoNum;
+    pDigigitoNum=malloc(sizeof(int));
+
+    while(*pN+precisionDecimal==nullptr){
+            pN--;
+        }
+
+    do{
+        *pDigito=*pN;
+        pN++;
+
+        if(*pDigito==('A') || *pDigito==('B') || *pDigito==('C') || *pDigito==('D') || *pDigito==('E') || *pDigito==('F')){
+            *pDigigitoNum-= 'A' +10;
+        }else{
+            if(*pDigito==('a') || *pDigito==('b') || *pDigito==('c') || *pDigito==('d') || *pDigito==('e') || *pDigito==('e')){
+               *pDigigitoNum-= 'a' +10;
+               }else{
+            *pDigigitoNum=*pDigito - '0';
+               }
+        }
+
+        potencia(&base,&pot, &cont);
+
+        *pResultadoNa10+=*pDigito / *pPot;
+        cont++;
+    }while(pN<pN+precisionDecimal+1)
 
     free(pPot);
     free(pDigito);
--- main.c
+++ main.c
@@ -1,8 +1,93 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define precision 16
+#define precisionEntera 10
+#define precisionDecimal 5
 
 /*PROYECTO OdC "convert"*/
+
+void divideNumero(char *n, char *nE, char *nD);
+
 int main() {
-    printf("Hello world!\n");
+    char *numero[precision];
+    numero=malloc(precision * sizeof(char));
+
+    char *numEntero[precisionEntera];
+    numEntero=malloc(precisionEntera * sizeof(char));
+
+    char *numDecimal[precisionDecimal];
+    numDecimal=malloc(precisionDecimal * sizeof(char));
+
+    int *baseOrigen;
+    baseOrigen=malloc(sizeof(int));
+
+    int *baseDestino;
+    baseDestino=malloc(sizeof(int));
+
+    int *cont;
+    cont=malloc(sizeof(int));
+
+    char *resultado[precision];
+    resultado=malloc(precision * sizeof(char));
+
+    int*numConvertido;
+    numConvertido=malloc(sizeof(int));
+
+
+    printf("Ingresar numero a ser cambiado de base\n");
+    scanf("%s", *numero);
+    printf("Ingresar base origen\n");
+    scanf("%d", *baseOrigen);
+    printf("Ingresar base destino\n");
+    scanf("%d", *baseDestino);
+
+    control();
+
+    divideNumero(&numero,&numEntero,&numDecimal);
+
+    switch(*baseDestino,*baseOrigen){
+        case *baseDestino==10:
+            deNa10Entero(&baseOrigen,&numEntero,&numConvertido);
+            deNa10Decimal(&baseOrigen,&numDecimal,&numConvertido);
+            break;
+        case *baseOrigen==10:
+            break;
+        case(*baseDestino!=10 && *baseOrigen!=10):
+            break;
+    }
+
+    free(numero);
+    free(numEntero);
+    free(numDecimal);
+    free(baseOrigen);
+    free(baseDestino);
+    free(cont);
+    free(resultado);
+    free(numConvertido);
+}
+}
     return 0;
+}
+
+void control(char *num, int *baseO){
+    if((*num - '0'< *baseO) || () )
+}
+
+void divideNumero(char *n, char *nE, char *nD){
+    int *contador;
+    contador=malloc(sizeof(int));
+    do{
+        *nE=*n;
+        n++;
+        nE++;
+    }while(*n!=".")
+
+    n++;
+
+    for(*contador=0;*contador<precisionDecimal;*contador++){
+        *n=*nD;
+        n++;
+        nD++;
+    }
+    free(contador);
 }
